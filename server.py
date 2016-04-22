360503475733a13fdb72c21178add95d582ab346
Steven Mahana <stevenmahana@gmail.com>
2016-04-22 06:39:39 -0700

    commit

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
 import time
 import zmq
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
 
+"""
 while True:
     #  Wait for next request from client
     message = socket.recv()
@@ -15,3 +13,28 @@ while True:
 
     #  Send reply back to client
     socket.send(b"World")
+"""
+
+class Events(object):
+
+    @staticmethod
+    def receive_message():
+
+        context = zmq.Context()
+        socket = context.socket(zmq.REP)
+        socket.bind("tcp://*:5555")
+
+        while True:
+            #  Wait for next request from client
+            message = socket.recv()
+            print("Received request: %s" % message)
+
+            #  Do some 'work'
+            time.sleep(1)
+
+            #  Send reply back to client
+            socket.send(b"World")
+
+if __name__ == '__main__':
+    zs = Events()
+    zs.receive_message()
